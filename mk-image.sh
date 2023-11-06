e8250d2f276cb3e9ec2829be7c1d042d06485a52
Scott <scott@starcases.com>
2023-11-06 07:00:42 -0500

    mess with docker/k8s

--- mk-image.sh
+++ mk-image.sh
@@ -7,6 +7,6 @@ image=gcr.io/distroless/java17-debian11
 workingctr=java17-debian11-working-container
 
 buildah from $image
-buildah copy $workingctr run-ctr.sh lib/* ptk/target/*.jar  /tmp
-buildah config --entrypoint ["/bin/java @tmp/run-ctr.sh"] $workingctr
+buildah copy $workingctr Podman-java-args lib/* ptk/target/*.jar  /tmp
+buildah config --entrypoint ["/bin/java","@Podman-java-args"] $workingctr
 buildah commit $workingctr ptk
